c6e652cef26227ae5c349e63faa803f9d0af3d69
daviddlw <davidjefiny@gmail.com>
2013-06-24 18:53:32 +0800

    MVC项目迁入

---
+++ DavidMVC/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DavidMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private int executeIndex = 1;
+        private IList<string> executeInfoLs = new List<string>();
+        private string executeInfo = string.Empty;
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.ExcuteLs = executeInfoLs;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            executeInfo = string.Format("{0}，OnAuthorization!", executeIndex);
+            executeInfoLs.Add(executeInfo);
+            executeIndex++;
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            executeInfo = string.Format("{0}，Before Action Execute!", executeIndex);
+            executeInfoLs.Add(executeInfo);
+            executeIndex++;
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            executeInfo = string.Format("{0}，After Action Execute!", executeIndex);
+            executeInfoLs.Add(executeInfo);
+            executeIndex++;
+        }
+
+        protected override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            executeInfo = string.Format("{0}，Before Result Execute!", executeIndex);
+            executeInfoLs.Add(executeInfo);
+            executeIndex++;
+        }
+
+        /// <summary>
+        /// 此时页面元素已经Render完毕
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            executeInfo = string.Format("{0}，After Result Execute!", executeIndex);
+            executeInfoLs.Add(executeInfo);
+            executeIndex++;
+        }
+    }
+}
